59b38fa199537a3aea2373739bb86d9a22da40c0
孟晓文 <nova_mengxw@126.com>
2018-05-08 10:52:52 +0800

    init

---
+++ src/query.js
@@ -0,0 +1,35 @@
+
+
+class Query {
+
+  constructor(data) { 
+    
+  }
+
+  static version = '__VERSION__'
+
+  // where
+  where() { }
+
+  like() { }
+
+  in() { }
+
+  between() { }
+
+  skip() { }
+
+  limit() { }
+
+  desc() { }
+
+  asc() { }
+
+  groupby()
+
+  find() { }
+
+  findOne() { }
+
+  destroy() { }
+}
